af1313ebbadb862144f51319e85dcee2f811ae4b
mmrahman01 <34527332+mmrahman01@users.noreply.github.com>
2018-12-14 23:57:42 -0600

    Use of variable types

--- variables.java
+++ variables.java
@@ -6,13 +6,65 @@
 
 public class variables{
 	
-	public static String clVar = " I am Class Variable"; // Class variable because of Static word, common to all instances
+	// Class variable because of Static word, common to all instances
+	// uses public static in front of the variable
+	// outside of the class accessed className.varName
+	
+	public static String clVar = " Class Var"; 
+	
+	
+	// without the public static infront, this is just instances
+	// each new instances (objects) have their own copy,
+	// unlike the class variable on the top, that is common for all objects
+	
+	String instanceVar = " Instance Var";
+	
+	
+	 public void myMethod(){
+    // local variable
+    	String myVar = "Inside Method";
+    	System.out.println(myVar);
+   }
+	
 	
 	public static void main (String[] args){
 		
+		// creating new objects, classType varName = new classType();
 		
-		System.out.println("Hello World");
+		variables object1 = new variables();
+		variables object2 = new variables();
+
+	System.out.println(object1.clVar);      	// prints Class Var
+	System.out.println(object2.clVar);			// prints Class Var
+	System.out.println(object1.instanceVar);	// prints Instance Var
+	System.out.println(object1.instanceVar);	// prints Instance Var
+	
+	object1.clVar ="Changed Class";
+	Object1.instanceVar = "Changed Instance Var";
+	
+	// even though object1 changed the class var, both the objects
+	// will change the value (Class variable) but only Object1 instance
+	// .. var will change but not the object2 (instance variable)
+	
+	
+	System.out.println(object1.clVar);      	// prints Changed Class
+	System.out.println(object2.clVar);			// prints Changed Class
+	System.out.println(object1.instanceVar);	// prints Changed Instance Var
+	System.out.println(object1.instanceVar);	// prints Instance Var
+	
+	object1.myMethod();							// will print the local variable
+												//... inside method
+	
+	
+	
+
+	
+		
 		
 	} //end main
 	
 }
+
+
+
+
