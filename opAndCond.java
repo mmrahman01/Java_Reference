0c95734cdd4ca26fff9af3bded92fc7d9a7052d5
mmrahman01 <34527332+mmrahman01@users.noreply.github.com>
2018-12-15 02:04:01 -0600

    Operator, conditionals logical

---
+++ opAndCond.java
@@ -0,0 +1,53 @@
+// c. Operator in Java
+
+/* 	1) Basic Arithmetic Operators : +, -, *, /, %
+	2) Assignment Operators :  =, +=, -=, *=, /=, %= , ( num2 -= num1) is num2 = num2 - num1
+	3) Auto-increment and Auto-decrement Operators : ++,--
+	4) Logical Operators : &&, ||, !
+	5) Comparison (relational) operators : ==, !=, >, <, >=, <=
+	6) Bitwise Operators : &, |, ^, ~, <<, >>
+	7) Ternary Operator : variable num1 = (expression) ? value if true : value if false
+
+    The signed left shift operator "<<" shifts a bit pattern to the left
+    The signed right shift operator ">>" shifts a bit pattern to the right.
+    The unsigned right shift operator ">>>" shifts a zero into the leftmost position
+ */
+
+
+// operator precedence
+
+/*
+Unary Operators  Multiplicative  Additive  Shift            Relational		Equality 	Bitwise AND
+++  – –  !  ~    *  / %          +  –      <<  >>  >>>      >  >=  <  <=	==  !=		&
+
+Bitwise XOR	 Bitwise OR 	Logical AND		Logical OR		Ternary
+^				|				&&				||				?:
+
+Assignment
+=  +=  -=  *=  /=  %=  >  >=  <  <=  &=  ^=  |= 
+
+*/
+
+
+public class opAndCond{
+
+
+public static void main (String[] args){
+
+
+// if, if else, if else if , nested if and switch;
+// logical operator
+
+
+
+
+
+
+}
+
+}
+
+
+
+
+
