de0a1e7a1597e95ce56c768806e493b5ae25c905
mmrahman01 <34527332+mmrahman01@users.noreply.github.com>
2018-12-14 21:25:05 -0600

    Static, Instance, Local

---
+++ variables.java
@@ -0,0 +1,18 @@
+// name the file same as the public class
+// javac file name makes the bytecode
+// JVM then runs this code 
+// data_type variable_name = value;
+// Three types of variables in Java, Static, Instance and Local.
+
+public class variables{
+	
+	public static String clVar = " I am Class Variable"; // Class variable because of Static word, common to all instances
+	
+	public static void main (String[] args){
+		
+		
+		System.out.println("Hello World");
+		
+	} //end main
+	
+}
